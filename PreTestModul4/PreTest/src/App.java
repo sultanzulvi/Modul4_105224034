4ec9f67fe9b5dab9a81e162609e65765c55d96e9
sultanzulvi <akundump27@gmail.com>
2026-03-31 14:46:11 +0700

    pretest

---
+++ PreTestModul4/PreTest/src/App.java
@@ -0,0 +1,19 @@
+class mahasiswa {
+    String nama;
+    int umur;
+    String jurusan;
+
+    void studi(){
+        System.out.println(name + "mahasiswa baru" );
+    }
+}
+
+public class app{
+    public static void main(String[] args){
+        mahasiswa mhsBaru = new mahasiswa();
+        mshBaru.nama = "Sultan Zulvi Risda";
+        mhsBaru.umur = 21;
+        mhsBaru.jurusanjurusan = "Ilmu Komputer";
+        mhsBaru.studi();
+    }
+}
